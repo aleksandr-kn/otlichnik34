867d5d1c92566161a7237909f02dbf54a3a0e630
aleksandr-kn <a.kanygin99@gmail.com>
2022-01-27 16:12:26 +0300

    йцу

--- js/main.js
+++ js/main.js
@@ -1,104 +1,47 @@
 $(function () {
-  //action on menu button click
-  $(".header__btn").on("click", function () {
-    $(".rightside-menu").removeClass("rightside-menu--close");
-  });
-
-  //action on menu button close
-  $(".rightside-menu__close").on("click", function () {
-    $(".rightside-menu").addClass("rightside-menu--close");
-  });
-
-  //top slider carousel
   $(".top__slider").slick({
-    dots: true,
     arrows: false,
-    fade: true,
+    dots: true,
     autoplay: true,
-  });
-
-  //contact page slider
-  $(".contact-slider").slick({
-    slidesToShow: 10,
-    slidesToScroll: 10,
-    dots: true,
-    arrows: false,
+    fade: true,
     responsive: [
       {
-        breakpoint: 1700,
+        breakpoint: 1200,
         settings: {
-          slidesToShow: 8,
-          slidesToScroll: 8,
+          dots: false,
         },
       },
-      {
-        breakpoint: 1511,
-        settings: {
-          slidesToShow: 6,
-          slidesToScroll: 6,
-        },
-      },
-      {
-        breakpoint: 1201,
-        settings: {
-          slidesToShow: 4,
-          slidesToScroll: 4,
-        },
-      },
-      {
-        breakpoint: 841,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        },
-      },
-      {
-        breakpoint: 551,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-        },
-      },
-      {
-        breakpoint: 376,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          autoplay: true,
-          autoplaySpeed: 2000,
-        },
-      },
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
     ],
   });
 
-  //blog page slider
-
-  $(".article-slider__box").slick({
-    arrows: true,
-    prevArrow:
-      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="slide left button"/></button>',
-    nextArrow:
-      '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="slide left button"/></button>',
+  $(".reviews__slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1141,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 846,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 586,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
-  //mobile menu button click
-  $(".header__btn-menu").on("click", function () {
-    $(".menu").toggleClass("menu--open");
-  });
-
-  //works-path section media query
-  if ($(window).width() < 661) {
-    $(".works-path__item--measurements").insertAfter(
-      $(".works-path__item--consultation")
-    );
-  }
-
-  var mixer = mixitup(".gallery__inner", {
-    load: {
-      filter: ".living",
-    },
+  $(".menu__btn").on("click", function () {
+    $(".header .menu").toggleClass("menu--active");
   });
 });
