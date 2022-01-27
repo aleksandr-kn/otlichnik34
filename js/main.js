315ac43461a5f8fc93b1625d9b34c2b3c6e0edf3
aleksandr-kn <a.kanygin99@gmail.com>
2022-01-27 15:53:23 +0300

    34

--- js/main.js
+++ js/main.js
@@ -1,47 +1,104 @@
 $(function () {
-  $(".top__slider").slick({
-    arrows: false,
-    dots: true,
-    autoplay: true,
-    fade: true,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          dots: false,
-        },
-      },
-    ],
+  //action on menu button click
+  $(".header__btn").on("click", function () {
+    $(".rightside-menu").removeClass("rightside-menu--close");
   });
 
-  $(".reviews__slider").slick({
-    arrows: false,
+  //action on menu button close
+  $(".rightside-menu__close").on("click", function () {
+    $(".rightside-menu").addClass("rightside-menu--close");
+  });
+
+  //top slider carousel
+  $(".top__slider").slick({
     dots: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    autoplay: true,
+  });
+
+  //contact page slider
+  $(".contact-slider").slick({
+    slidesToShow: 10,
+    slidesToScroll: 10,
+    dots: true,
+    arrows: false,
     responsive: [
       {
-        breakpoint: 1141,
+        breakpoint: 1700,
+        settings: {
+          slidesToShow: 8,
+          slidesToScroll: 8,
+        },
+      },
+      {
+        breakpoint: 1511,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+        },
+      },
+      {
+        breakpoint: 1201,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 841,
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 3,
         },
       },
       {
-        breakpoint: 846,
+        breakpoint: 551,
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 2,
         },
       },
       {
-        breakpoint: 586,
+        breakpoint: 376,
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 2000,
         },
       },
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
     ],
   });
 
-  $(".menu__btn").on("click", function () {
-    $(".header .menu").toggleClass("menu--active");
+  //blog page slider
+
+  $(".article-slider__box").slick({
+    arrows: true,
+    prevArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="slide left button"/></button>',
+    nextArrow:
+      '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="slide left button"/></button>',
+  });
+
+  //mobile menu button click
+  $(".header__btn-menu").on("click", function () {
+    $(".menu").toggleClass("menu--open");
+  });
+
+  //works-path section media query
+  if ($(window).width() < 661) {
+    $(".works-path__item--measurements").insertAfter(
+      $(".works-path__item--consultation")
+    );
+  }
+
+  var mixer = mixitup(".gallery__inner", {
+    load: {
+      filter: ".living",
+    },
   });
 });
