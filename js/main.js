be7dbbfff6d8aaab7b91b27d00678c38a9b2a565
aleksandr-kn <a.kanygin99@gmail.com>
2022-01-27 16:29:25 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,47 +1,28 @@
 $(function () {
-  $(".top__slider").slick({
+  $(".slider").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
     arrows: false,
     dots: true,
-    autoplay: true,
-    fade: true,
     responsive: [
       {
-        breakpoint: 1200,
-        settings: {
-          dots: false,
-        },
-      },
-    ],
-  });
-
-  $(".reviews__slider").slick({
-    arrows: false,
-    dots: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1141,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 846,
+        breakpoint: 1140,
         settings: {
           slidesToShow: 2,
+          slidersToScroll: 2,
         },
       },
       {
-        breakpoint: 586,
+        breakpoint: 769,
         settings: {
           slidesToShow: 1,
+          slidersToScroll: 1,
         },
       },
     ],
   });
 
   $(".menu__btn").on("click", function () {
-    $(".header .menu").toggleClass("menu--active");
+    $(".menu__list").toggleClass("menu__list--active");
   });
 });
