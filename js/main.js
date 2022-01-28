dc2cf4c272d4b5671b08f2fe3ab9e2e7e2d44dc5
aleksandr-kn <a.kanygin99@gmail.com>
2022-01-28 13:10:03 +0300

    Правки

--- js/main.js
+++ js/main.js
@@ -22,6 +22,33 @@ $(function () {
     ],
   });
 
+  $(".reviews__slider").slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1141,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 846,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 586,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  }); 
+
   $(".menu__btn").on("click", function () {
     $(".menu__list").toggleClass("menu__list--active");
   });
